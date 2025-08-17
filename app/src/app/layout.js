58da268dc1c2750e83f294151d8c9e6cd6953fed
ryuvanraj <yuvan.r2005@gmail.com>
2025-08-17 15:26:04 +0530

    UI Enhancing

---
+++ app/src/app/layout.js
@@ -0,0 +1,35 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { WalletProvider } from "./contexts/WalletContext";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "DroneDash - Web3 Drone Delivery on Sei",
+  description: "Decentralized drone delivery platform powered by Sei blockchain with smart contract escrow and Eliza OS intelligence",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <WalletProvider>
+          {children}
+        </WalletProvider>
+      </body>
+    </html>
+  );
+}
